4eea4f3ea98f3b634469663992a4fb1a2e11f10f
Ricardo Gonçalves <tome.wave@gmail.com>
2018-12-19 15:42:15 +0000

    Update travis config to run `strap.sh`

--- bin/strap.sh
+++ bin/strap.sh
@@ -102,21 +102,25 @@ fi
 logk
 
 # Check and enable full-disk encryption.
-logn "Checking full-disk encryption status:"
-if fdesetup status | grep $Q -E "FileVault is (On|Off, but will be enabled after the next restart)."; then
-  logk
-elif [ -n "$STRAP_CI" ]; then
-  echo
-  logn "Skipping full-disk encryption for CI"
-elif [ -n "$STRAP_INTERACTIVE" ]; then
-  echo
-  log "Enabling full-disk encryption on next reboot:"
-  sudo fdesetup enable -user "$USER" \
-    | tee ~/Desktop/"FileVault Recovery Key.txt"
-  logk
+if [ "$SKIP_FILE_ENCRYPTION" = true ]; then
+  logn "Skipping full-disk encryption, since \$SKIP_FILE_ENCRYPTION is set to false"
 else
-  echo
-  abort "Run 'sudo fdesetup enable -user \"$USER\"' to enable full-disk encryption."
+  logn "Checking full-disk encryption status:"
+  if fdesetup status | grep $Q -E "FileVault is (On|Off, but will be enabled after the next restart)."; then
+    logk
+  elif [ -n "$STRAP_CI" ]; then
+    echo
+    logn "Skipping full-disk encryption for CI"
+  elif [ -n "$STRAP_INTERACTIVE" ]; then
+    echo
+    log "Enabling full-disk encryption on next reboot:"
+    sudo fdesetup enable -user "$USER" \
+      | tee ~/Desktop/"FileVault Recovery Key.txt"
+    logk
+  else
+    echo
+    abort "Run 'sudo fdesetup enable -user \"$USER\"' to enable full-disk encryption."
+  fi
 fi
 
 # Install the Xcode Command Line Tools.
