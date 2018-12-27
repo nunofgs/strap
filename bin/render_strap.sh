6194ad8b29a0c96bd24fc55b9fb2a4be7a86bf70
Nuno Sousa <nuno@uphold.com>
2018-12-27 14:37:33 +0000

    Add tests for bash script

--- bin/render_strap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-OUTPUT_FILE=bin/strap2.sh
-
-ejs-cli bin/strap.sh -O '{ "STRAP_GIT_NAME": "First Last", "STRAP_GIT_EMAIL": "user@uphold.com", "STRAP_GITHUB_USER": "user", "STRAP_GITHUB_TOKEN": "token", "STRAP_ISSUES_URL": "https://github.com/uphold/strap/issues/new", "STRAP_GITHUB_ORGANIZATION_OR_USERNAME": "ricardobcl", "STRAP_DOTFILES": "dotfiles-1" }' > $OUTPUT_FILE
-chmod +x $OUTPUT_FILE
-
